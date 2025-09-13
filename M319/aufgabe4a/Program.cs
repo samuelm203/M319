a10c3a7a328c5d2261550c89e3af124815ac1679
Samuel Ming <155850886+samuelm203@users.noreply.github.com>
2025-09-13 14:05:26 +0200

    Add files via upload

---
+++ M319/aufgabe4a/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        uint numberFromUser;
+        uint numberRoundedIn100;
+
+        byte differenceToValue = 50; 
+
+
+
+        Console.WriteLine("Geben sie eine ganze Zahl ein, im Bereich von 0 und 100'000");
+        numberFromUser = Convert.ToUInt32(Console.ReadLine());
+
+        numberRoundedIn100 = numberFromUser / 100 * 100;
+
+
+
+        if (numberFromUser % 100 >= 50)
+        {
+            numberRoundedIn100 += 100;
+        } 
+
+        Console.WriteLine("Die Zahl gerundet auf Hunderter ist: " + numberRoundedIn100);
+    }
+}
