c4b68757981c4e34c7f710a14d71120e82c0b8d7
Samuel Ming <155850886+samuelm203@users.noreply.github.com>
2025-09-15 16:58:52 +0200

    Add files via upload

---
+++ M319/aufgabe5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float bodyweightInKilo;
+        float heightInMeters; 
+
+        Console.WriteLine("Enter your bodyweight in kilo :");
+        bodyweightInKilo = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter your height in meter :");
+        heightInMeters = float.Parse(Console.ReadLine());
+
+        //Ausrechnen des BMI
+        float bmi = bodyweightInKilo / (heightInMeters * heightInMeters);
+
+        //BMI ausgeben  und auf eine Nachkommastelle runden
+        Console.WriteLine("Your BMI is : " + Math.Round(bmi, 1));
+
+        //Zusatz: BMI-Kategorie
+        if (bmi < 18.5)
+        {
+            Console.WriteLine("You are underweight.");
+        }
+        else if (bmi >= 18.5 && bmi < 25)
+        {
+            Console.WriteLine("You have a normal weight.");
+        }
+        else if (bmi >= 25 && bmi < 30)
+        {
+            Console.WriteLine("You are overweight.");
+        }
+        else
+        {
+            Console.WriteLine("You are obese.");
+        }
+    }
+
+}
