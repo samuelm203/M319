e0e12b4c7fc01a495971c5d408f61962eb7b272c
Samuel Ming <155850886+samuelm203@users.noreply.github.com>
2025-10-13 17:07:16 +0200

    Add files via upload

---
+++ M319/aufgabe9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Geben Sie die erreichte Punktzahl ein");
+        int pointsUser = Convert.ToInt32(Console.ReadLine());
+
+        if (pointsUser > 100)
+        {
+            Console.WriteLine("Die Punktzahl darf nicht über 100 liegen");
+        } 
+        else if (pointsUser >= 90)
+        {
+            Console.WriteLine("Sehr gut");
+        }
+        else if (pointsUser >= 80)
+        {
+            Console.WriteLine("Gut");
+        }
+        else if (pointsUser >= 70)
+        {
+            Console.WriteLine("Befriedigend");
+        }
+        else if (pointsUser >= 60)
+        {
+            Console.WriteLine("Ausreichend");
+        }
+        else if (pointsUser <= 59)
+        {
+            Console.WriteLine("Nicht bestanden");
+        }
+        else
+        {
+            Console.WriteLine("Die Punktzahl darf nicht unter 0 liegen");
+        }
+    }
+}
+
