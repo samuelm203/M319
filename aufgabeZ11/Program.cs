bd5bba6add39f4e0d14bc277e4d8c017e77ca8e0
Samuel Ming <155850886+samuelm203@users.noreply.github.com>
2025-11-14 16:17:36 +0100

    Add files via upload

---
+++ aufgabeZ11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bitte geben Sie einen Monat ein (1-12): ");
+        int monat = Convert.ToInt32(Console.ReadLine());
+
+        string jahreszeit;
+
+        if (monat < 1 || monat > 12)
+        {
+            jahreszeit = "Ungültige Eingabe!";
+        }
+        else if (monat == 12 || monat == 1 || monat == 2)
+        {
+            jahreszeit = "Winter";
+        }
+        else if (monat >= 3 && monat <= 5)
+        {
+            jahreszeit = "Frühling";
+        }
+        else if (monat >= 6 && monat <= 8)
+        {
+            jahreszeit = "Sommer";
+        }
+        else
+        {
+            jahreszeit = "Herbst";
+        }
+
+        Console.WriteLine("Der Monat gehört zur Jahreszeit: " + jahreszeit);
+    }
+}
