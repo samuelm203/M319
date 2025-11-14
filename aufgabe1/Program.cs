7eaaad8716f4b2b0fd19f8551829e684f7fa477e
Samuel Ming <155850886+samuelm203@users.noreply.github.com>
2025-11-14 16:16:16 +0100

    Add files via upload

---
+++ aufgabe1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        // Initialisierung von Variablen
+        string firstName; 
+        string lastName; 
+        int age; 
+
+        Console.WriteLine("What is your first name?"); // Vorname abfragen
+        firstName = Console.ReadLine(); // Usereingabe wird in Vorname-Variable gespeichert
+
+        Console.WriteLine("What is your last name?"); // Nachname abfragen
+        lastName = Console.ReadLine(); // Usereingabe wird in Nachname-Variable gespeichert
+
+        Consoe.WriteLine("How old are you?"); // Alter abfragen
+        age = Convert.ToInt32(Console.ReadLine()); // Initialisierung Alter-Variable
+
+        age = age + 1; // Alter um 1 erhöhen
+
+        Console.WriteLine("Hello " + firstName + " " + lastName + ". Bald wirst du " + age ); // Ausgabe der Begrüßung
+
+        Console.ReadKey(); // Konsolenfenster offen halten
+    }
+}
---
+++ aufgabe4b/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        uint numberFromUser;
+        uint numberRoundedIn50;
+
+        Console.WriteLine("Geben sie eine ganze Zahl ein, im Bereich von 0 und 100'000");
+        numberFromUser = Convert.ToUInt32(Console.ReadLine());
+
+        // Aufrunden auf das nächste fünziger, falls nötig
+        numberRoundedIn50 = ((numberFromUser + 25) / 50) * 50;
+
+        Console.WriteLine("Die Zahl gerundet auf Fünfziger ist: " + numberRoundedIn50);
+    }
+}
