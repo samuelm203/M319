ff149cfa2a26790f6d8fa6f0daba6c352d0abefe
Samuel Ming <155850886+samuelm203@users.noreply.github.com>
2025-12-01 10:18:32 +0100

    Add files via upload

---
+++ AufgabeZ19/AufgabeZ19/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string userPassword;
+
+        Console.WriteLine("Gib dein Passwort ein: ");
+        userPassword = Console.ReadLine();
+
+        IsPasswordGood(userPassword);
+
+        if (IsPasswordGood(userPassword))
+        {
+            Console.WriteLine("Du hast ein gutes Passwort"); 
+        } else
+        {
+            Console.WriteLine("Dein Passwort ist nicht gut"); 
+        }
+
+    }
+
+    static bool IsPasswordGood(string userPassword)
+    {
+        if (userPassword.Length < 8)
+        {
+            return false;
+        }
+
+        bool hasDigit = false;
+        bool hasUpper = false;
+
+        Regex regexSpecialChar = new Regex("[!§\\$%&/()=?@#*\\+\\.,;:\\-_<>{}\\[\\]\\^~|]");
+        bool hasSpecialChar = regexSpecialChar.IsMatch(userPassword);
+
+        foreach (char c in userPassword)
+        {
+            if (Char.IsDigit(c))
+            {
+                hasDigit = true;
+            }
+            else if (Char.IsUpper(c))
+            {
+                hasUpper = true;
+            }
+
+            if (hasDigit && hasUpper && hasSpecialChar)
+            {
+                break;
+            }
+        }
+
+        return hasDigit && hasUpper && hasSpecialChar;
+    }
+}
---
+++ aufgabeZ20/aufgabeZ20/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace aufgabeZ20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie heissen Sie?");
+            string name = Console.ReadLine();
+
+            greeting(name);
+        }
+
+        static string greeting(string name)
+        {
+            bool isTimeDependent = false;
+            int hour = 0;
+
+            Console.WriteLine("Was ist die gewünschte Sprache [de, en, fr]?");
+            string language = Console.ReadLine().ToLower();
+
+            if (name == "" || name == null)
+            {
+                switch (language)
+                {
+                    case "de":
+                        name = "Gast";
+                        break;
+                    case "en":
+                        name = "Guest";
+                        break;
+                    case "fr":
+                        name = "Client";
+                        break;
+                    default:
+                        name = "Gast";
+                        language = "de";
+                        break;
+                }
+            }
+
+            Console.WriteLine("Wünschen Sie eine zeitabhängige Begrüssung? (ja/nein)");
+            string timeDependent = Console.ReadLine()?.ToLower();
+
+            if (timeDependent == "ja")
+            {
+                isTimeDependent = true;
+                hour = DateTime.Now.Hour;
+            }
+
+            switch (language)
+            {
+                case "de":
+                    if (isTimeDependent)
+                    {
+                        if (hour >= 4 && hour < 12)
+                            Console.WriteLine("Guten Morgen " + name);
+                        else if (hour >= 12 && hour < 18)
+                            Console.WriteLine("Guten Tag " + name);
+                        else if (hour >= 18 && hour < 22)
+                            Console.WriteLine("Guten Abend " + name);
+                        else
+                            Console.WriteLine("Gute Nacht " + name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Guten Tag " + name);
+                    }
+                    break;
+
+                case "fr":
+                    if (isTimeDependent)
+                    {
+                        if (hour >= 4 && hour < 18)
+                            Console.WriteLine("Bonjour " + name);
+                        else if (hour >= 18 && hour < 22)
+                            Console.WriteLine("Bonsoir " + name);
+                        else
+                            Console.WriteLine("Bonne Nuit " + name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bonjour " + name);
+                    }
+                    break;
+
+                case "en":
+                    if (isTimeDependent)
+                    {
+                        if (hour >= 4 && hour < 12)
+                            Console.WriteLine("Good morning " + name);
+                        else if (hour >= 12 && hour < 18)
+                            Console.WriteLine("Hello " + name);
+                        else if (hour >= 18 && hour < 22)
+                            Console.WriteLine("Good evening " + name);
+                        else
+                            Console.WriteLine("Good night " + name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hello " + name);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Guten Tag " + name);
+                    break;
+            }
+
+            return name;
+        }
+    }
+}
