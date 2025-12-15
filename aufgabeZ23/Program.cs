dc579ac56f7386f70da91608891d24d79a3276a7
Samuel Ming <155850886+samuelm203@users.noreply.github.com>
2025-12-15 17:14:38 +0100

    Add files via upload

---
+++ aufgabeZ23/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aufgabeZ26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Unteraufgabe 1
+            int[] caseOne = { 1, 2, 3, 5, 2, 32, 231, 325, 23, 10 };
+
+            for (int i = 0; i < caseOne.Length; i++)
+            {
+                Console.WriteLine(caseOne[i]);
+            }
+
+            // Unteraufgabe 2
+            Console.WriteLine("-----");
+
+            int[] caseTwo = { 4, 7, 2, 10, 15 };
+
+            Console.WriteLine(caseTwo[4]);
+
+            // Unteraufgabe 3
+            Console.WriteLine("-----");
+            int[] caseThree = new int[10];
+
+            for (int i = 0; i < caseThree.Length; i++)
+            {
+                caseThree[i] = i * 2;
+                Console.WriteLine("Zahl " + i + ": " + caseThree[i]);
+            }
+
+            // Unteraufgabe 4
+            Console.WriteLine("-----");
+
+            int [] caseFour = { 3, 6, 9, 12, 15, 18, 21, 24, 27, 30 };
+
+            int sumCaseFour = caseFour.Sum();
+
+            Console.WriteLine("Summe: " + sumCaseFour);
+
+            // Unteraufgabe 5
+            Console.WriteLine("-----");
+
+            int[] caseFive = { 5, 10, 15, 2320, 25, 30, 35, 40, 45, 50 };
+
+            int biggestCaseFive = caseFive.Max();
+
+            Console.WriteLine("Grösste Zahl: " + biggestCaseFive);
+        }
+    }
+
+}
---
+++ aufgabeZ24/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aufgabeZ24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                double[] doubleArray = new double[5];
+
+                for (int i = 0; i < doubleArray.Length; i++)
+                {
+                    Console.Write($"Bitte geben Sie Zahl {i + 1} ein: ");
+                    string input = Console.ReadLine();
+
+                    if (double.TryParse(input, out double result))
+                    {
+                        doubleArray[i] = result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ungültige Eingabe. Bitte eine Zahl eingeben.");
+                        i--;
+                    }
+                }
+
+                double sum = doubleArray.Sum();
+                double average = sum / doubleArray.Length;
+
+                average = Math.Round(average, 1);
+
+                Console.WriteLine($"Der Durchschnitt der eingegebenen Werte ist: {average}");
+            
+        }
+    }
+}
